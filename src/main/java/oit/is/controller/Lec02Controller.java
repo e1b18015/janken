15a4f008ff2b50a83a135ee4543df330d3aa32f7
e1b18015 <e1b18015@oit.ac.jp>
2020-10-09 18:32:10 +0900

    lec02の作成

--- src/main/java/oit/is/controller/Lec02Controller.java
+++ src/main/java/oit/is/controller/Lec02Controller.java
@@ -5,14 +5,28 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Lec02Controller {
   @PostMapping("/lec02")
-  public String lec02(@RequestParam String name, ModelMap model) {
+  public String lec02(@PathVariable String name, ModelMap model) {
     model.addAttribute("playerName", name);
     return "lec02.html";
   }
-  
+
+  @GetMapping("/hoi")
+  public String hoi(@PathVariable String hand, ModelMap model) {
+    String winner = "You Lose...";
+    if (hand.equals("Gu")) {
+      winner = "Draw";
+    }
+    if (hand.equals("Pa")) {
+      winner = "You Win!!";
+    }
+    model.addAttribute("yourHand", hand);
+    model.addAttribute("enemyHand", "Gu");
+    model.addAttribute("jankenResult", winner);
+    return "lec02.html";
+  }
+
 }
