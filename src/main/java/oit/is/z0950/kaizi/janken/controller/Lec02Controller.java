a0310075d8bd5fd75164feade87db0e5bbfb6ba9
e1b18015 <e1b18015@oit.ac.jp>
2020-10-13 19:08:34 +0900

    ユーザ認証にlec02を対応

--- src/main/java/oit/is/z0950/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0950/kaizi/janken/controller/Lec02Controller.java
@@ -1,23 +1,36 @@
 package oit.is.z0950.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z0950.kaizi.janken.model.Room;
+
 @Controller
+@RequestMapping("/lec02")
 public class Lec02Controller {
 
-  @PostMapping("/lec02")
-  public String lec02(@RequestParam String name, ModelMap model) {
-    String name1 = name;
-    model.addAttribute("playerName", name1);
+  @Autowired
+  private Room room;
+
+  @RequestMapping()
+  public String lec02(Principal prin, ModelMap model, ModelMap model2) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model2.addAttribute("loginUser", loginUser);
+    model.addAttribute("room", this.room);
+
     return "lec02.html";
   }
 
   @GetMapping("/hoi")
-  public String hoi(@RequestParam String hand, ModelMap model1, ModelMap model2, ModelMap model3) {
+  public String hoi(@RequestParam String hand, ModelMap model1, ModelMap model2, ModelMap model3, ModelMap model4,
+      ModelMap model5, Principal prin) {
     String winner = "You Lose...";
     if (hand.equals("Gu")) {
       winner = "Draw";
@@ -28,6 +41,10 @@ public class Lec02Controller {
     model1.addAttribute("yourHand", hand);
     model2.addAttribute("enemyHand", "Gu");
     model3.addAttribute("jankenResult", winner);
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model5.addAttribute("loginUser", loginUser);
+    model4.addAttribute("room", this.room);
     return "lec02.html";
   }
 
