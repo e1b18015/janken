8d7d970aa73bd382aaf35c258007307bbed52e12
e1b18015 <e1b18015@oit.ac.jp>
2020-11-02 15:45:46 +0900

    DBにmatch_infoテーブルを作成

--- src/main/java/oit/is/z0950/kaizi/janken/model/MatchMapper.java
+++ src/main/java/oit/is/z0950/kaizi/janken/model/MatchMapper.java
@@ -9,10 +9,10 @@ import org.apache.ibatis.annotations.Insert;
 
 @Mapper
 public interface MatchMapper {
-  @Select("SELECT id,user_1,user_2,user_1_hand,user_2_hand from matches")
+  @Select("SELECT id,user_1,user_2,user_1_hand,user_2_hand,is_active from matches")
   ArrayList<Match> selectAll();
 
-  @Insert("INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand) VALUES (#{user_1},#{user_2},#{user_1_hand},#{user_2_hand});")
+  @Insert("INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand,is_active) VALUES (#{user_1},#{user_2},#{user_1_hand},#{user_2_hand});")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertMatch(Match Match);
 }
